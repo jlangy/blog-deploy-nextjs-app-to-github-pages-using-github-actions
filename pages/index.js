6c2ed7ac21e01750e44adaa34aaa0910befc480e
Jonathan Langlois <jon@button.is>
2021-06-23 20:10:18 -0700

    est

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <p>Link to the article: <a
-        href="https://lukaszwozniak.dev/blog/deploy-nextjs-app-to-github-pages-using-github-actions">Deploy NextJS app to
-        GitHub Pages using GitHub Actions</a>.</p>
+      <h1>Test</h1>
     </main>
   )
 }
